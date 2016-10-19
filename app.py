355722401cfcb9f31b11b0d5959d28eba5d51a1b
DESKTOP-TVFEE04\Admin <nguyenquocviet1306@gmail.com>
2016-10-19 20:46:40 +0700

    no message

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
+post1 = {
+    "title" : "Good day",
+    "content" : "I met a girl. She had black eyes and loves dog"
+}
+
+
+post2 = {
+    "title" : "Bad day",
+    "content" : "I met a gay"
+}
+
+print(post1["title"])
+print(post1["content"])
+
+posts = [post1,post2]
 
 @app.route('/')
-def hello_world():
-    return 'Hello Techkids!'
+def main():
+    return json.dumps(posts)
 
 
 if __name__ == '__main__':
